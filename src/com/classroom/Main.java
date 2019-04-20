f34897b791a87108d3130e8d1838c8ab04e9e0cf
Powerfuldeveloper <thepowerfuldeveloper@gmail.com>
2019-04-20 12:30:36 +0430

    sub class style

--- src/com/classroom/Main.java
+++ src/com/classroom/Main.java
@@ -6,87 +6,13 @@ public class Main {
 
     public static void main(String[] args) {
         ToDoList toDoList = new ToDoList();
-        toDoList.add(new ToDo("test1"));
-        toDoList.add(new ToDo("TEST2"));
-        toDoList.add(new ToDo("TEST3"));
-        toDoList.add(new ToDo("TEST4"));
-        ToDo l;
+        toDoList.add(new ToDoList.ToDoListArray.ToDo("test1"));
+        toDoList.add(new ToDoList.ToDoListArray.ToDo("TEST2"));
+        toDoList.add(new ToDoList.ToDoListArray.ToDo("TEST3"));
+        toDoList.add(new ToDoList.ToDoListArray.ToDo("TEST4"));
+        ToDoList.ToDoListArray.ToDo l;
         while(toDoList.hasNext())
             System.out.println(toDoList.next());
-    }
-}
-
-class ToDo {
-    String name;
-    String description;
-
-    public ToDo() {
-    }
-
-    public ToDo(String name) {
-        this(name, "");
-    }
-
-    public ToDo(String name, String description) {
-        this.name = name;
-        this.description = description;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    @Override
-    public String toString() {
-        return "ToDo{" +
-                "name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                '}';
-    }
-}
-
-class ToDoListArray {
-
-    private ToDo[] toDos;
-    private int current_add = 0;
-
-    private int current_limit = 1;
-
-    public ToDoListArray() {
-        this.toDos = new ToDo[current_limit];
-    }
-
-    int size() {
-        return toDos.length;
-    }
-
-    ToDo get(int index) {
-        return toDos[index];
-    }
-
-    int last_index() {
-        return current_add;
-    }
-
-    void add(ToDo toDo) {
-        if (current_add >= current_limit) {
-            current_limit *= 2;
-            toDos = Arrays.copyOf(toDos, current_limit);
-        }
-        toDos[current_add] = toDo;
-        current_add++;
     }
 }
 
@@ -94,21 +20,21 @@ class ToDoList {
     private ToDoListArray toDoListArray = new ToDoListArray();
     private int pivot = -1;
 
-    ToDo get(int index) {
+    ToDoListArray.ToDo get(int index) {
         return toDoListArray.get(index);
     }
 
-    ToDo get(){
+    ToDoListArray.ToDo get(){
         if (hasNext())
             return toDoListArray.get(pivot);
         return null;
     }
 
-    void add(ToDo toDo) {
+    void add(ToDoListArray.ToDo toDo) {
         toDoListArray.add(toDo);
     }
 
-    ToDo next() {
+    ToDoListArray.ToDo next() {
         if (hasNext()) {
             pivot++;
             return toDoListArray.get(pivot);
@@ -118,5 +44,79 @@ class ToDoList {
 
     boolean hasNext() {
         return pivot + 1 < toDoListArray.last_index();
+    }
+
+    static class ToDoListArray {
+
+        private ToDo[] toDos;
+        private int current_add = 0;
+
+        private int current_limit = 1;
+
+        public ToDoListArray() {
+            this.toDos = new ToDo[current_limit];
+        }
+
+        int size() {
+            return toDos.length;
+        }
+
+        ToDo get(int index) {
+            return toDos[index];
+        }
+
+        int last_index() {
+            return current_add;
+        }
+
+        void add(ToDo toDo) {
+            if (current_add >= current_limit) {
+                current_limit *= 2;
+                toDos = Arrays.copyOf(toDos, current_limit);
+            }
+            toDos[current_add] = toDo;
+            current_add++;
+        }
+
+        static class ToDo {
+            String name;
+            String description;
+
+            public ToDo() {
+            }
+
+            public ToDo(String name) {
+                this(name, "");
+            }
+
+            public ToDo(String name, String description) {
+                this.name = name;
+                this.description = description;
+            }
+
+            public String getName() {
+                return name;
+            }
+
+            public void setName(String name) {
+                this.name = name;
+            }
+
+            public String getDescription() {
+                return description;
+            }
+
+            public void setDescription(String description) {
+                this.description = description;
+            }
+
+            @Override
+            public String toString() {
+                return "ToDo{" +
+                        "name='" + name + '\'' +
+                        ", description='" + description + '\'' +
+                        '}';
+            }
+        }
     }
 }
